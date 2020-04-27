8cd58cf5c8b315f689ca20720a81851e3a6817ba
Ujjwal Singh <ujjwal.singh@hallibnurton.com>
2020-04-27 19:57:33 +0530

    uncompress string

---
+++ interview_questions/uncompress-string.py
@@ -0,0 +1,24 @@
+
+def expand(): 
+   compressed = "[ab[c]]” 
+   stack s = stack()  
+   factor = 1
+   for char in compressed: 
+       if char != ‘]’: 
+           s.push(char) ['[', 'a', 'b','[', 'c']
+        elif char == "]" : 
+            temp = list   
+            # S # [‘A’, ‘2’, ’[‘, ‘b’, ‘c’] 
+            while s.peek() != ‘[‘:
+                temp.append(s.pop()) # ['c'] 
+                # S # [‘A’, ‘2’, ’[‘] 
+                temp.reverse() 
+                # Temp # ['a', ‘b’, ‘c’] 
+                s.pop() # pops left ‘[‘ 
+                factor  = isdigit(s.peek())?; s.pop(): 1 
+                intermediate_obj = "".join(temp)*factor #  
+                s.push(intermediate_obj) #  ['[', 'a', 'b','c']
+
+                # [a,b,c] 
+    
+    return  "".join(s)  
